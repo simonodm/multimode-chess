e85b6ff9c391fbc3dc3c7778c96468eb7e0b0aa0
Dmitry Simonov <dmitry@dmitrysimonov.com>
2021-08-14 13:23:43 +0200

    Form icon added

--- ChessGUI/MainForm.Designer.cs
+++ ChessGUI/MainForm.Designer.cs
@@ -29,15 +29,17 @@ namespace ChessGUI
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(24)))));
             this.ClientSize = new System.Drawing.Size(1200, 800);
-            this.Name = "Chess";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
             this.Text = "Chess";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- ChessGUI/Properties/Resources.Designer.cs
+++ ChessGUI/Properties/Resources.Designer.cs
@@ -161,6 +161,16 @@ namespace ChessGUI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon queen_light1 {
+            get {
+                object obj = ResourceManager.GetObject("queen_light1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap rook_dark {
